c1594449426a3082e62e9fd855b48dbcc6053db4
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-12-22 20:36:07 -0300

    Create and give a like in tweets!

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3333;
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3333;
 
-app.get('/', (req, res) => {
-  return res.send('Hello World');
-});
+mongoose.connect(
+  'mongodb+srv://goweek:goweek@cluster0-mge5c.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+app.use(express.json())
+app.use(require('./routes'));
 
 app.listen(port, () => {
   console.log('Server runing on port: ', port);
